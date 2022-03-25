3bd14eafe792025d13202b2b6e54a38daea9aab9
Courtney Dozier <79492432+cadozie1@users.noreply.github.com>
2022-03-25 15:28:48 -0700

    updating again

--- setup/create_tables.sql
+++ setup/create_tables.sql
@@ -5,59 +5,59 @@ CREATE TABLE Nation(
 );
 
 CREATE TABLE Year(
-	yearkey INTEGER  NOT NULL,
-	PRIMARY KEY(yearkey)  NOT NULL
+	yearkey INTEGER NOT NULL,
+	PRIMARY KEY(yearkey)
 );
 
 CREATE TABLE Finances(
-	nationkey CHAR(3)  NOT NULL,
-	yearkey INTEGER  NOT NULL,
+	nationkey CHAR(3) NOT NULL,
+	yearkey INTEGER NOT NULL,
 	ppp DOUBLE NOT NULL,
-	unemployment DOUBLE  NOT NULL,
-	gini_coefficient DOUBLE  NOT NULL,
-	PRIMARY KEY (nationkey, yearkey)  NOT NULL,
+	unemployment DOUBLE NOT NULL,
+	gini_coefficient DOUBLE NOT NULL,
+	PRIMARY KEY (nationkey, yearkey),
 	FOREIGN KEY (nationkey) REFERENCES Nation
 	FOREIGN KEY (yearkey) REFERENCES Year
 );
 
 CREATE TABLE NetCon(
-	nationkey CHAR(3)  NOT NULL,
-	yearkey INTEGER  NOT NULL,
-	internet_users DOUBLE  NOT NULL,
-	cell_subscriptions INTEGER  NOT NULL,
-	PRIMARY KEY (nationkey, yearkey)  NOT NULL,
-	FOREIGN KEY (nationkey) REFERENCES Nation
+	nationkey CHAR(3) NOT NULL,
+	yearkey INTEGER NOT NULL,
+	internet_users DOUBLE NOT NULL,
+	cell_subscriptions INTEGER NOT NULL,
+	PRIMARY KEY (nationkey, yearkey),
+	FOREIGN KEY (nationkey) REFERENCES Nation,
 	FOREIGN KEY (yearkey) REFERENCES Year
 );
 
 CREATE TABLE QoL(
-	nationkey CHAR(3)  NOT NULL,
-	yearkey INTEGER  NOT NULL,
-	child_deaths DOUBLE  NOT NULL,
-	sanitation_usage DOUBLE  NOT NULL,
+	nationkey CHAR(3) NOT NULL,
+	yearkey INTEGER NOT NULL,
+	child_deaths DOUBLE NOT NULL,
+	sanitation_usage DOUBLE NOT NULL,
 	PRIMARY KEY (nationkey, yearkey)  NOT NULL,
-	FOREIGN KEY (nationkey) REFERENCES Nation
+	FOREIGN KEY (nationkey) REFERENCES Nation,
 	FOREIGN KEY (yearkey) REFERENCES Year
 );
 
 CREATE TABLE Sustainability(
-	nationkey CHAR(3)  NOT NULL,
-	yearkey INTEGER  NOT NULL,
+	nationkey CHAR(3) NOT NULL,
+	yearkey INTEGER NOT NULL,
 	yearly_CO2 DOUBLE NOT NULL,
-	forest_land DOUBLE  NOT NULL,
-	PRIMARY KEY (nationkey, yearkey)  NOT NULL,
-	FOREIGN KEY (nationkey) REFERENCES Nation
+	forest_coverage DOUBLE NOT NULL,
+	PRIMARY KEY (nationkey, yearkey),
+	FOREIGN KEY (nationkey) REFERENCES Nation,
 	FOREIGN KEY (yearkey) REFERENCES Year
 );
 
 CREATE TABLE Energy(
-	nationkey CHAR(3)  NOT NULL,
-	yearkey INTEGER  NOT NULL,
+	nationkey CHAR(3) NOT NULL,
+	yearkey INTEGER NOT NULL,
 	nuclear_electricity DOUBLE NOT NULL,
-	SDI <datatype>  NOT NULL,
+	SDI <datatype> NOT NULL,
 	gas_prices <datatype> NOT NULL,
-	PRIMARY KEY (nationkey, yearkey)  NOT NULL,
-	FOREIGN KEY (nationkey) REFERENCES Nation
+	PRIMARY KEY (nationkey, yearkey),
+	FOREIGN KEY (nationkey) REFERENCES Nation,
 	FOREIGN KEY (yearkey) REFERENCES Year
 );
 
@@ -66,17 +66,17 @@ CREATE TABLE Weather(
 	yearkey INTEGER  NOT NULL,
 	extreme_heat <datatype>  NOT NULL,
 	global_temperatures <datatype>  NOT NULL,
-	PRIMARY KEY (nationkey, yearkey)  NOT NULL,
-	FOREIGN KEY (nationkey) REFERENCES Nation
+	PRIMARY KEY (nationkey, yearkey),
+	FOREIGN KEY (nationkey) REFERENCES Nation,
 	FOREIGN KEY (yearkey) REFERENCES Year
 );
 
 CREATE TABLE Finances(
-	nationkey CHAR(3)  NOT NULL,
-	yearkey INTEGER  NOT NULL,
-	child_population  <datatype>  NOT NULL,
-	population <datatype>  NOT NULL,
-	PRIMARY KEY (nationkey, yearkey)  NOT NULL,
-	FOREIGN KEY (nationkey) REFERENCES Nation
+	nationkey CHAR(3) NOT NULL,
+	yearkey INTEGER NOT NULL,
+	child_population  <datatype> NOT NULL,
+	population <datatype> NOT NULL,
+	PRIMARY KEY (nationkey, yearkey),
+	FOREIGN KEY (nationkey) REFERENCES Nation,
 	FOREIGN KEY (yearkey) REFERENCES Year
 );
